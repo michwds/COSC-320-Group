0a408bf314c964a7b105d189183c26f6188ec99e
Michwds <michael.woods@live.ca>
2023-03-18 19:30:48 -0700

    removed unnessecary declaration

--- a2.py
+++ a2.py
@@ -1,7 +1,5 @@
 
 def loop(tweets, keywords):
-    newText = ""			                                                    #initialise the “sublist”
-    #map = {keys: keywords} - using "keywords" input instead 	                #populate hashmap with key value pairs
     for tweet in tweets:		                                                #iterates each tweet in tweets list 
         newText = " ".join(keywords.get(word, word) for word in tweet.text.split())      #search and replace using hashmap             
         tweet.text = newText			                                            #replace tweet text with processed string
