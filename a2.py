dbd657ebd682be14db13cd74584020449f0b5805
Michwds <michael.woods@live.ca>
2023-03-18 19:29:06 -0700

    fixed whitespace errors in core loop

--- a2.py
+++ a2.py
@@ -3,7 +3,6 @@ def loop(tweets, keywords):
     newText = ""			                                                    #initialise the “sublist”
     #map = {keys: keywords} - using "keywords" input instead 	                #populate hashmap with key value pairs
     for tweet in tweets:		                                                #iterates each tweet in tweets list 
-        words = tweet.text.split()	                                            #delimits full text into a list of words
-    newText = " ".join(keywords.get(word, word) for word in tweet.text.split())      #search and replace using hashmap             
-    tweet.text = newText			                                            #replace tweet text with processed string
+        newText = " ".join(keywords.get(word, word) for word in tweet.text.split())      #search and replace using hashmap             
+        tweet.text = newText			                                            #replace tweet text with processed string
     return tweets 				                                                #after repeating for all tweets, return output
