7e9e06c31b576fc38759fed7f75caf9c6cf02cf5
Michwds <michael.woods@live.ca>
2023-03-18 19:37:01 -0700

    -fixed formatting to match sample dataset -added documentation

--- a1.py
+++ a1.py
@@ -1,14 +1,14 @@
 
-def loop(tweets, keywords):
+def loop(tweets, keywords):                 #tweets is a list of tweet objects, keywords is a list of key objects each with key-value pair
     newText = ""			                #initialise the “sublist”
     for tweet in tweets:			            #iterates each tweet in tweets list 
-        words = tweet.text.split()	        #delimits full text into a list of words
+        words = tweet.content.split()	        #delimits full text into a list of words
         for idx, word in enumerate(words): 		#iterates each word in the list
             for key in keywords:	           #iterates each keyword in the abbreviation list
-                if word.txt == key.name: 	#checks to see if the word is a keyword
-                    word.txt = key.txt 	    #replaces abbreviation with full text
+                if word == key.name: 	#checks to see if the word is a keyword
+                    word = key.txt 	    #replaces abbreviation with full text
             if idx > 0:		                #add a space between words for the “sublist”
                 newText += " "
             newText += word	                #construct the replacement text “sublist”
-    tweet.text = newText			        #replace tweet text with processed string
+    tweet.content = newText			        #replace tweet text with processed string
     return tweets 				            #after repeating for all tweets, return output
--- a2.py
+++ a2.py
@@ -1,6 +1,6 @@
 
-def loop(tweets, keywords):
+def loop(tweets, keywords):      #takes a list of tweet objects and keywords dictionary as inputs
     for tweet in tweets:		                                                #iterates each tweet in tweets list 
-        newText = " ".join(keywords.get(word, word) for word in tweet.text.split())      #search and replace using hashmap             
-        tweet.text = newText			                                            #replace tweet text with processed string
+        newText = " ".join(keywords.get(word, word) for word in tweet.content.split())      #search and replace using hashmap             
+        tweet.content = newText			                                            #replace tweet text with processed string
     return tweets 				                                                #after repeating for all tweets, return output
