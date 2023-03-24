550863e5bb4ee644f3296ae0921e3ae19c1f1158
Michwds <michael.woods@live.ca>
2023-03-24 13:12:37 -0700

    moved for loop outside of main process for easier plotting

--- a1.py
+++ a1.py
@@ -26,22 +26,23 @@ with open('tweets.csv', mode='r') as csv_file:
         tweets.append(Tweet(row['content']))
 
 
-def loop(tweets, keywords):                 #tweets is a list of tweet objects, keywords is a list of key objects each with key-value pair
+def loop(tweet, keywords):                 #tweets is a list of tweet objects, keywords is a list of key objects each with key-value pair
     newText = ""			                #initialise the “sublist”
-    for tweet in tweets:			            #iterates each tweet in tweets list 
-        words = tweet.content.split()	        #delimits full text into a list of words
-        for idx, word in enumerate(words): 		#iterates each word in the list
-            for key in keywords:	           #iterates each keyword in the abbreviation list
-                if word == key.name: 	#checks to see if the word is a keyword
-                    word = key.txt 	    #replaces abbreviation with full text
-            if idx > 0:		                #add a space between words for the “sublist”
-                newText += " "
-            newText += word	                #construct the replacement text “sublist”
-        tweet.content = newText			        #replace tweet text with processed string
-        newText=""
-    return tweets 				            #after repeating for all tweets, return output
+    words = tweet.content.split()	        #delimits full text into a list of words
+    for idx, word in enumerate(words): 		#iterates each word in the list
+        for key in keywords:	           #iterates each keyword in the abbreviation list
+            if word == key.name: 	#checks to see if the word is a keyword
+                word = key.txt 	    #replaces abbreviation with full text
+        if idx > 0:		                #add a space between words for the “sublist”
+            newText += " "
+        newText += word	                #construct the replacement text “sublist”
+    tweet.content = newText			        #replace tweet text with processed string
+    newText=""
+    return tweet 				            #after repeating for all tweets, return output
 
-processed_tweets = loop(tweets, keywords)
+processed_tweets = []
+for tweet in tweets:
+    processed_tweets.append(loop(tweet, keywords))
 
 # Print the processed tweets
 for tweet in processed_tweets:
