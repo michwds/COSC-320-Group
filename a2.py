fe23a36a7667be6a1cbe347e1802b9c96015523b
Michwds <michael.woods@live.ca>
2023-04-11 13:30:15 -0700

    combined a1 and a2, added helper methods

--- a2.py
+++ a2.py
@@ -4,27 +4,58 @@ class Tweet:
     def __init__(self, content):
         self.content = content
 
+class Keyword:
+    def __init__(self, name, txt):
+        self.name = name
+        self.txt = txt
+def main():
+    keywords1 = []
+    with open('keywords_data.csv', mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Skip header row
+        for row in csv_reader:
+            keywords1.append(Keyword(row[0], row[1]))
 
-keywords = {}
-with open('keywords_data.csv', mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    next(csv_reader)  # Skip header row
-    for row in csv_reader:
-        keywords[row[0]] = row[1]
+    keywords2 = {}
+    with open('keywords_data.csv', mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Skip header row
+        for row in csv_reader:
+            keywords2[row[0]] = row[1]
 
-tweets = []
-with open('tweets.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        tweets.append(Tweet(row['content']))
+    tweets = []
+    with open('tweets.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            tweets.append(Tweet(row['content']))
+    
+    processed_tweets1 = process(a1, tweets, keywords1)
 
-def loop(tweets, keywords):      #takes a list of tweet objects and keywords dictionary as inputs
-    for tweet in tweets:		                                                #iterates each tweet in tweets list 
-        newText = " ".join(keywords.get(word, word) for word in tweet.content.split())      #search and replace using hashmap             
-        tweet.content = newText			                                            #replace tweet text with processed string
-    return tweets 				                                                #after repeating for all tweets, return output
+    processed_tweets2 = process(a2, tweets, keywords2)
 
-processed_tweets = loop(tweets, keywords)
+def a2(tweet, keywords):      #takes a list of tweet objects and keywords dictionary as inputs
+    newText = " ".join(keywords.get(word, word) for word in tweet.content.split())      #search and replace using hashmap             
+    tweet.content = newText			                                            #replace tweet text with processed string
+    return tweet 				                                                #after repeating for all tweets, return output
+
+def a1(tweet, keywords):                 #tweets is a list of tweet objects, keywords is a list of key objects each with key-value pair
+    newText = ""			                #initialise the “sublist”
+    words = tweet.content.split()	        #delimits full text into a list of words
+    for idx, word in enumerate(words): 		#iterates each word in the list
+        for key in keywords:	           #iterates each keyword in the abbreviation list
+            if word == key.name: 	#checks to see if the word is a keyword
+                word = key.txt 	    #replaces abbreviation with full text
+        if idx > 0:		                #add a space between words for the “sublist”
+            newText += " "
+        newText += word	                #construct the replacement text “sublist”
+    tweet.content = newText			        #replace tweet text with processed string
+    newText=""
+    return tweet 				            #after repeating for all tweets, return output
+
+def process(loop, tweets, keywords):
+    for tweet in tweets:
+        loop(tweet, keywords)
+    return tweets
 
 # Print the processed tweets
 n=1
